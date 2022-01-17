f2f9421d3be3cc90f24da6a21ee90bc4bfab4cb3
ouardaoui <ayoubouardaoui3@gmail.com>
2022-01-17 17:22:19 +0100

    first commit

---
+++ src/api/tracker.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const instance = axios.create({
+  baseURL: "https://pfe-track.herokuapp.com",
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
